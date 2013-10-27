f255cc2bdb2b1a87ccdb0b85a405f0bb815b55a2
pussbb <pussbb@gmail.com>
2013-10-27 18:07:54 +0200

    fix

---
+++ sx/exceptions.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+"""
+
+from __future__ import print_function, unicode_literals, with_statement, \
+    absolute_import
+
+__author__ = 'pussbb'
+
+
+class ScalixException(Exception):
+    """ Base Exception"""
+    pass
+
+class ScalixLicenseError(ScalixException):
+    """An exception type used to indicate a problem reading,
+    validating, or installing the license.
+
+    """
+    pass
+
+class ScalixProcessingException(ScalixException):
+    """An exception to be used as a trigger to reset state in a function.
+    For example, if a variable may need reset to "None" or the function
+    return if any of several problems occur, rather than handle each
+    individually, they can be grouped into a try/catch block.
+
+    """
+    pass
+
+class ScalixCommandNotFoundException(ScalixException):
+    """An CommandNotFound exception so we can trap it
+
+    """
+    def __init__(self, command):
+        self.message = "Command not found {0}".format(command)
+
+class ScalixExternalCommandFailed(Exception):
+    """
+    Raised by :py:func:`execute()` when an external command returns with a
+    nonzero exit code.
+    """
