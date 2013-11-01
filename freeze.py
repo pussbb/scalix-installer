167956e0f3b68031a1b5d6ed29842ce8d36b94f9
pussbb <pussbb@gmail.com>
2013-11-01 16:10:19 +0200

    freeze package add

---
+++ freeze.py
@@ -0,0 +1,26 @@
+
+from bbfreeze import Freezer
+from sx.system import System
+system = System()
+
+include = (
+    "sx",
+    '__future__',
+    'rpm',
+    'os',
+    'sys',
+    'distutils',
+    'docopt',
+    'time',
+    'logging',
+    'subprocess',
+    'pipes',
+    'platform',
+    'webbrowser',
+    'socket',
+    're',
+)
+f = Freezer("merlin.%s" % system.arch , includes=include)
+f.addScript("merlin.py")
+
+f()
